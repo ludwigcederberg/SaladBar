06e700baa4d4dbace40ffda5f4cdaa2ae02e9d12
Ludwig Cederberg <lu0007ce-s@student.lu.se>
2023-10-10 10:44:35 +0200

    list of ids to flatmap

--- project/src/fetchMovies.js
+++ project/src/fetchMovies.js
@@ -5,7 +5,7 @@
     console.log(keywords);
     const fetches = await Promise.all(keywords.map(keyword=> safeFetch("https://www.omdbapi.com/?s=" + keyword + "&apikey=351c6f1f")));
     const imdbID = fetches.map(x=>x.Search.map(y=>y.imdbID));
-    const movieInfo = await Promise.all(imdbID[0].map(id => safeFetch("https://www.omdbapi.com/?i=" + id + "&apikey=351c6f1f")));
+    const movieInfo = await Promise.all(imdbID.flat().map(id => safeFetch("https://www.omdbapi.com/?i=" + id + "&apikey=351c6f1f")));
     console.log(movieInfo);
   }
 
