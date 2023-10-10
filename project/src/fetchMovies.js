c30f6f87ea0a6a70d50a7412db6c1275a731500a
Ludwig Cederberg <lu0007ce-s@student.lu.se>
2023-10-10 10:23:35 +0200

    fetchMovies

--- project/src/fetchMovies.js
+++ project/src/fetchMovies.js
@@ -4,7 +4,9 @@
   export default async function getMovies(keywords, genre){
     console.log(keywords);
     const fetches = await Promise.all(keywords.map(keyword=> safeFetch("https://www.omdbapi.com/?s=" + keyword + "&apikey=351c6f1f")));
-    console.log(fetches);
+    const imdbID = fetches.map(x=>x.Search.map(y=>y.imdbID));
+    const movieInfo = await Promise.all(imdbID[0].map(id => safeFetch("https://www.omdbapi.com/?i=" + id + "&apikey=351c6f1f")));
+    console.log(movieInfo);
   }
 
   async function safeFetch(url){
