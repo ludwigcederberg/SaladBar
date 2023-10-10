d2f87e02f625576d6825fe1117e3ede086662218
Jesper <jesper1519@outlook.com>
2023-10-10 11:41:35 +0200

    yo

--- project/src/fetchMovies.js
+++ project/src/fetchMovies.js
@@ -13,7 +13,7 @@
     return fetch(url)
     .then(response => {
       if(!response.ok) {
-        throw new Error('${url} returned status ${response.status}');
+        throw new Error(`${url} returned status ${response.status}`);
       }
       return response.json();
     });
